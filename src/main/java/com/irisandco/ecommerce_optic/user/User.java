0f1fa7309289245ea67d7efab31d945c147dff26
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-07 17:36:26 +0200

    feat(User): add Lombok annotations

--- src/main/java/com/irisandco/ecommerce_optic/user/User.java
+++ src/main/java/com/irisandco/ecommerce_optic/user/User.java
@@ -2,9 +2,15 @@ package com.irisandco.ecommerce_optic.user;
 
 import com.irisandco.ecommerce_optic.cart.Cart;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name= "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,9 +28,6 @@ public class User {
     @OneToOne(mappedBy = "user")
     private Cart cart;
 
-    public User() {
-    }
-
     public User(Long id, String username, String email, String password) {
         this.id = id;
         this.username = username;
@@ -36,38 +39,6 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setCart(Cart cart) {
-        this.cart = cart;
     }
 
     public void addCart(Cart cart) {

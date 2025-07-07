9bc3906a8d407940f6b9bd6d90ee72fb8a53ec18
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-07-07 17:33:47 +0200

    feat(Category): add Lombok annotations

--- src/main/java/com/irisandco/ecommerce_optic/category/Category.java
+++ src/main/java/com/irisandco/ecommerce_optic/category/Category.java
@@ -2,12 +2,19 @@ package com.irisandco.ecommerce_optic.category;
 
 import com.irisandco.ecommerce_optic.product.Product;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "categories")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -20,14 +27,6 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private List<Product> products = new ArrayList<>();
 
-    public Category(){}
-
-    public Category(Long id, String name, List<Product> products) {
-        this.id = id;
-        this.name = name;
-        this.products = products;
-    }
-
     public Category(String name) {
         this.name = name;
     }
@@ -36,23 +35,6 @@ public class Category {
         this.id = id;
         this.name = name;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
     public void addProducts(Product product) {
         this.products.add(product);
     }
